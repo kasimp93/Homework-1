2cbf1ce3bc56fa6beb8e1c1901bb3d24a3284758
Kasim <kasimp93@bu.edu>
2016-09-17 14:40:36 -0400

    I have added a new python program

---
+++ w1a_limits.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  8 12:05:27 2016
+
+@author: kasim
+"""
+
+x = "{:<6} {:<22} {:<22} {:<22}"
+
+print(x.format('Bytes', 'Largest Unsigned Int', 'Minimun Signed Int', 'Maximum Signed Int'))
+
+b = 1
+
+for num in range (1,5):
+    largest = 2**(b*8)
+    minim = -2**(b*8-1)
+    maxim = 2**(b*8-1)-1
+    
+    print (x.format(b, largest, minim, maxim ))
+    b = b*2  
+    
+    
